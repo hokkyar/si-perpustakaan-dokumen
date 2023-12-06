01b04e7e794857f1e4d1415ddf4c138f5f88ce61
hokkyar <hokkyaryasta@gmail.com>
2023-12-07 00:35:56 +0700

    refactoring and minor edit to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,3 @@
-<?php
-if (isset($link)) {
-    $link = 'Upload';
-} else {
-    $link = 'Dashboard';
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -46,7 +39,7 @@ if (isset($link)) {
             <ul class="navbar-nav">
 
                 <li class="nav-item">
-                    <a class="nav-link @if ($link == 'Dashboard') active @endif" href="/dashboard">
+                    <a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" href="/dashboard">
                         <div
                             class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -63,7 +56,7 @@ if (isset($link)) {
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link @if ($link == 'Upload') active @endif" href="/upload">
+                    <a class="nav-link {{ Request::is('upload') ? 'active' : '' }}" href="/upload">
                         <div
                             class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -80,9 +73,34 @@ if (isset($link)) {
                                     </g>
                                 </g>
                             </svg>
-
                         </div>
                         <span class="nav-link-text ms-1">Upload Dokumen</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::is('settings') ? 'active' : '' }}" href="/settings">
+                        <div
+                            class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-cloud-arrow-up-fill" viewBox="0 0 16 16">
+                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <g transform="translate(-2319.000000, -291.000000)" fill="#FFFFFF"
+                                        fill-rule="nonzero">
+                                        <g transform="translate(1716.000000, 291.000000)">
+                                            <g transform="translate(603.000000, 0.000000)">
+                                                <path class="color-background"
+                                                    d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
+
+
+                                            </g>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
+
+                        </div>
+                        <span class="nav-link-text ms-1">Pengaturan</span>
                     </a>
                 </li>
 
